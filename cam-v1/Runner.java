5834fd7f5bd61057e163dac79fc43d6ebb3407d9
IsaiahP6 <Isaiah.memo.p@gmail.com>
2023-10-11 11:35:04 -0700

    virtual pet project

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,6 +1,33 @@
+import javax.swing.JOptionPane;
+
 public class Runner {
+    public Runner(){
+        
+        VirtualPet p = new VirtualPet();
+        p.exercise();
+        takeABeat(5000);
+        p.feed();
+
+    }
+    public void takeABeat(int milliseconds){
+            try {
+            Thread.sleep(milliseconds); //milliseconds
+        } catch(Exception e){
+
+        }
+    }
+    public String getAnswer(String q){
+        String s = (String)JOptionPane.showInputDialog(
+            new JFrame(),
+            message:"Your Message",
+            title:"Your Title",
+            JOptionPane.PLAIN_MESSAGE
+        );
+        return s;
+    }
     public static void main(String[] args) {
-        new VirtualPet();
+        new Runner();
     
     }
 }
+
