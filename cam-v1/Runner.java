212d49dad77f8d7cd74982f5adb6206031e83286
Isaiah Peetoom <Isaiah.memo.p@gmail.com>
2023-10-13 11:56:38 -0700

    virtual pet

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -8,12 +8,14 @@ public class Runner {
         //p.exercise();
         //takeABeat(5000);
         //p.feed();
-        String a = getAnswer("how was your day?", "");
-        if(a.equals("good")){
-            p.face.setMessage("that's nice");
-            p.face.setImage("ecstatic_2");
-        }
+        //String a = getAnswer("how was your day?", "");
+        //if(a.equals("good")){
+        //    p.face.setMessage("that's nice");
+        //    p.face.setImage("ecstatic_2");
+        //    System.out.println();
 
+        //}
+        answerBox();
     }
     public void takeABeat(int milliseconds){
             try {
@@ -21,6 +23,21 @@ public class Runner {
         } catch(Exception e){
 
         }
+    }
+    public int answerBox(){
+        Object[] options = {"milk",
+            "eggs"};
+
+        int n = JOptionPane.showOptionDialog(
+            new JFrame(),
+    "Should I buy milk or eggs",
+            "grocery shopping",
+            JOptionPane.YES_NO_CANCEL_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options,
+            options[1]);
+            return n;
     }
     public String getAnswer(String q, String t){
         String s = (String)JOptionPane.showInputDialog(
@@ -30,10 +47,12 @@ public class Runner {
             JOptionPane.PLAIN_MESSAGE
         );
         return s;
+
     }
+  
     public static void main(String[] args) {
         new Runner();
-    
+        
     }
 }
 
